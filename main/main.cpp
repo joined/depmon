bf61b5746095b792a587a02f3a0a7b3863b2fd66
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2023-10-27 17:50:10 +0200

    Drop captive portal. Provision with builtin manager. Use montserrat 4bpp everywhere. Small UI improvements

--- main/main.cpp
+++ main/main.cpp
@@ -4,105 +4,200 @@
 #include <esp_mac.h>
 #include <esp_timer.h>
 #include <esp_wifi.h>
+#include <freertos/event_groups.h>
 #include <mdns.h>
 #include <nvs_flash.h>
 #include <thread>
+#include <wifi_provisioning/manager.h>
+#include <wifi_provisioning/scheme_softap.h>
 
+#include "http_server.hpp"
 #include "lcd.h"
 #include "ui.hpp"
-#include "http_server.hpp"
-#include "dns_server.h"
+
+/* Signal Wi-Fi events on this event-group */
+const int WIFI_CONNECTED_EVENT = BIT0;
+static EventGroupHandle_t wifi_event_group;
+
+#define PROV_QR_VERSION "v1"
+#define PROV_TRANSPORT_SOFTAP "softap"
+#define PROV_MGR_MAX_RETRY_COUNT 3
 
 using namespace std::chrono_literals;
 using namespace std;
 
-// TODO The SSID shouldn't be constant, it should be different for each device
-static const string AP_SSID = "esp32";
-static const string AP_PASSWORD = "whatever";
-static const string PROVISIONING_QR_CODE_DATA = "WIFI:S:" + AP_SSID + ";T:WPA;P:" + AP_PASSWORD + ";;";
-
-static const char *WIFI_EVENT_HANDLER_TAG = "wifi_event_handler";
-
-static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
-        ESP_LOGI(WIFI_EVENT_HANDLER_TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac), event->aid);
-    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data;
-        ESP_LOGI(WIFI_EVENT_HANDLER_TAG, "station " MACSTR " leave, AID=%d", MAC2STR(event->mac), event->aid);
+/* Event handler for catching system events */
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    static int retries;
+    if (event_base == WIFI_PROV_EVENT) {
+        switch (event_id) {
+        case WIFI_PROV_START:
+            ESP_LOGI(TAG, "Provisioning started");
+            break;
+        case WIFI_PROV_CRED_RECV: {
+            wifi_sta_config_t *wifi_sta_cfg = (wifi_sta_config_t *)event_data;
+            ESP_LOGI(TAG,
+                     "Received Wi-Fi credentials"
+                     "\n\tSSID     : %s\n\tPassword : %s",
+                     (const char *)wifi_sta_cfg->ssid, (const char *)wifi_sta_cfg->password);
+            break;
+        }
+        case WIFI_PROV_CRED_FAIL: {
+            wifi_prov_sta_fail_reason_t *reason = (wifi_prov_sta_fail_reason_t *)event_data;
+            ESP_LOGE(TAG,
+                     "Provisioning failed!\n\tReason : %s"
+                     "\n\tPlease reset to factory and retry provisioning",
+                     (*reason == WIFI_PROV_STA_AUTH_ERROR) ? "Wi-Fi station authentication failed"
+                                                           : "Wi-Fi access-point not found");
+            retries++;
+            if (retries >= PROV_MGR_MAX_RETRY_COUNT) {
+                ESP_LOGI(TAG, "Failed to connect with provisioned AP, reseting provisioned credentials");
+                wifi_prov_mgr_reset_sm_state_on_failure();
+                retries = 0;
+            }
+            break;
+        }
+        case WIFI_PROV_CRED_SUCCESS:
+            ESP_LOGI(TAG, "Provisioning successful");
+            retries = 0;
+            break;
+        case WIFI_PROV_END:
+            /* De-initialize manager once provisioning is finished */
+            wifi_prov_mgr_deinit();
+            break;
+        default:
+            break;
+        }
+    } else if (event_base == WIFI_EVENT) {
+        switch (event_id) {
+        case WIFI_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case WIFI_EVENT_STA_DISCONNECTED:
+            ESP_LOGI(TAG, "Disconnected. Connecting to the AP again...");
+            esp_wifi_connect();
+            break;
+        case WIFI_EVENT_AP_STACONNECTED:
+            ESP_LOGI(TAG, "SoftAP transport: Connected!");
+            break;
+        case WIFI_EVENT_AP_STADISCONNECTED:
+            ESP_LOGI(TAG, "SoftAP transport: Disconnected!");
+            break;
+        default:
+            break;
+        }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "Connected with IP Address:" IPSTR, IP2STR(&event->ip_info.ip));
+        /* Signal main application to continue execution */
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_EVENT);
     }
 }
 
+static void wifi_init_sta(void) {
+    /* Start Wi-Fi in station mode */
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+/** Will have format `PROV_2A3B4C` */
+static void get_provisioning_ssid(char *service_name, size_t max) {
+    uint8_t eth_mac[6];
+    const char *ssid_prefix = "PROV_";
+    esp_wifi_get_mac(WIFI_IF_STA, eth_mac);
+    snprintf(service_name, max, "%s%02X%02X%02X", ssid_prefix, eth_mac[3], eth_mac[4], eth_mac[5]);
+}
+
+static void wifi_prov_print_qr(const char *name) {
+    if (!name) {
+        ESP_LOGW(TAG, "Cannot generate QR code payload. Data missing.");
+        return;
+    }
+    char payload[150] = {0};
+    snprintf(payload, sizeof(payload),
+             "{\"ver\":\"%s\",\"name\":\"%s\""
+             ",\"transport\":\"%s\"}",
+             PROV_QR_VERSION, name, PROV_TRANSPORT_SOFTAP);
+    logs_screen.addQRCode(payload);
+}
+
 extern "C" void app_main(void) {
+    LVGL_LCD::init();
+    UIManager::init();
+
     /* Initialize NVS partition */
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         /* NVS partition was truncated and needs to be erased */
         ESP_ERROR_CHECK(nvs_flash_erase());
-
         /* Retry nvs_flash_init */
         ESP_ERROR_CHECK(nvs_flash_init());
     }
 
     /* Initialize TCP/IP */
-    // TODO Maybe we need to use a public IP for the captive portal to work on Samsung devices? https://github.com/CDFER/Captive-Portal-ESP32/blob/main/src/main.cpp#L18
     ESP_ERROR_CHECK(esp_netif_init());
 
     // Create the default event loop
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_event_group = xEventGroupCreate();
 
+    /* Register our event handlers for Wi-Fi/IP related events */
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_PROV_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    /* Initialize Wi-Fi including netif with default config */
+    esp_netif_create_default_wifi_sta();
     esp_netif_create_default_wifi_ap();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+    /* Configuration for the provisioning manager */
+    wifi_prov_mgr_config_t config = {.scheme = wifi_prov_scheme_softap,
+                                     .scheme_event_handler = WIFI_PROV_EVENT_HANDLER_NONE};
 
-    wifi_config_t wifi_config = {
-        .ap =
-            {
-                // TODO Make ssid and password dynamic
-                .ssid = "esp32",
-                .password = "whatever",
-                .ssid_len = 0,
-                .channel = 1,
-                .authmode = WIFI_AUTH_WPA2_PSK,
-                .max_connection = 1,
-                .pmf_cfg =
-                    {
-                        .required = false,
-                    },
-            },
-    };
+    /* Initialize provisioning manager with the configuration parameters set above */
+    ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-
-    LVGL_LCD::init();
-
-    UIManager::init();
-
-    this_thread::sleep_for(2s);
+    bool provisioned = false;
+    /* Let's find out if the device is provisioned */
+    ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
 
     logs_screen.switchTo();
-    logs_screen.addLogLine("It looks like you're trying to set up your device.");
-    logs_screen.addLogLine("Please scan the following QR code to connect to the device:");
-    logs_screen.addQRCode(PROVISIONING_QR_CODE_DATA);
-    logs_screen.addLogLine("Alternatively, connect to the network \"" + AP_SSID + "\" with password \"" + AP_PASSWORD +
-                           "\".");
-    logs_screen.addLogLine("After connecting, please go to http://depmon.local to continue setup.");
-    // TODO Configure mDNS
 
-    ESP_ERROR_CHECK(esp_wifi_start());
+    if (!provisioned) {
+        ESP_LOGI(TAG, "Starting provisioning");
 
-    setup_http_server();
+        char service_name[12];
+        get_provisioning_ssid(service_name, sizeof(service_name));
 
-    dns_server_config_t config = DNS_SERVER_CONFIG_SINGLE("*" /* all A queries */, "WIFI_AP_DEF" /* softAP netif ID */);
-    start_dns_server(&config);
+        ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(WIFI_PROV_SECURITY_0, nullptr, service_name, nullptr));
+        logs_screen.addLogLine("It looks like you're trying to set up your device.");
+        logs_screen.addLogLine("Please download the ESP SoftAP Provisioning app from the App Store or Google Play, "
+                               "open it and follow the instructions.");
+        wifi_prov_print_qr(service_name);
+    } else {
+        ESP_LOGI(TAG, "Already provisioned, starting Wi-Fi STA");
+        logs_screen.addLogLine("Connecting to WiFi...");
 
-    // this_thread::sleep_for(5s);
+        /* We don't need the manager as device is already provisioned,
+         * so let's release it's resources */
+        wifi_prov_mgr_deinit();
 
-    // departures_screen.switchTo();
-    // for (int i = 0; i < 20; i++) {
-    //     departures_screen.addRandomDepartureItem();
-    // }
+        /* Start Wi-Fi station */
+        wifi_init_sta();
+    }
+
+    /* Wait for Wi-Fi connection */
+    xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_EVENT, true, true, portMAX_DELAY);
+
+    logs_screen.addLogLine("Connected to WiFi! Switching to departures board...");
+    this_thread::sleep_for(2s);
+
+    departures_screen.switchTo();
+    for (int i = 0; i < 20; i++) {
+        departures_screen.addRandomDepartureItem();
+    }
+
+    // setup_http_server();
 }
