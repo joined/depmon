a3d31791b4ce6c0d94f25e95870a323630436829
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2023-10-28 18:25:48 +0200

    Add MUI to frontend with some example. Gzip all frontend files. Use bigger partition.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
-import { h } from "preact";
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import { css } from '@emotion/css';
+import Button from '@mui/material/Button';
 
-const App = () => <h1>Hello world in Preact</h1>;
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className={css`padding: 8px;`}>
+            <h1 className={css`color: white`}>Hello world in Preact</h1>
+            <Button variant="contained" onClick={() => setCount((count) => count + 1)}>
+                Hello world {count}
+            </Button>
+        </div>
+    );
+};
 
 export default App;
