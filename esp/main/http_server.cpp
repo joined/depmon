39f7bd269efac47b827dbce74c91d03fafd1b608
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2023-11-04 13:14:07 +0100

    Move SPIFFS init to http server and minor refactorings

--- esp/main/http_server.cpp
+++ esp/main/http_server.cpp
@@ -2,6 +2,7 @@
 #include <esp_log.h>
 #include <esp_random.h>
 #include <esp_system.h>
+#include <esp_spiffs.h>
 #include <esp_app_desc.h>
 #include <esp_vfs.h>
 #include <fcntl.h>
@@ -17,6 +18,32 @@
 static char scratch[SCRATCH_BUFSIZE];
 
 static const char *TAG = "http_server";
+
+static esp_err_t init_fs(void) {
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/www", .partition_label = NULL, .max_files = 5, .format_if_mount_failed = false};
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return ESP_FAIL;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(NULL, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+    } else {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+    return ESP_OK;
+}
 
 /* Set HTTP response content type according to file extension */
 static esp_err_t set_content_type_from_file(httpd_req_t *req, const std::string &filepath) {
@@ -113,6 +140,7 @@ static esp_err_t api_get_version_handler(httpd_req_t *req)
 }
 
 httpd_handle_t setup_http_server() {
+    init_fs();
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.uri_match_fn = httpd_uri_match_wildcard;
     httpd_handle_t server = NULL;
