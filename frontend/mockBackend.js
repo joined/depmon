e6b7eb683d2a4c6122d88ed022d7d9f4e5294b27
Lorenzo Gasparini <gasparini.lorenzo@gmail.com>
2023-11-11 20:26:17 +0100

    Add station selection to frontend and mock backend server

--- frontend/mockBackend.js
+++ frontend/mockBackend.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let currentStationId = '900078102';
+
+app.use(express.json());
+
 app.get('/version', (req, res) => {
     res.send(
         JSON.stringify({
@@ -10,6 +14,23 @@ app.get('/version', (req, res) => {
             project_name: 'Mock project name',
             compile_time: 'Mock compile time',
             compile_date: 'Mock compile date',
+        })
+    );
+});
+
+app.get('/currentstation', (req, res) => {
+    res.send(
+        JSON.stringify({
+            id: currentStationId,
+        })
+    );
+});
+
+app.post('/currentstation', (req, res) => {
+    currentStationId = req.body.id;
+    res.send(
+        JSON.stringify({
+            ok: 1,
         })
     );
 });
